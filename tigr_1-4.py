0a02563ff6d4cb376cb788cf180b4ed1be791b83
Irina8820 <idplummy88@gmail.com>
2026-04-30 18:18:54 +0300

    Update tigr_1-4.py

--- tigr_1-4.py
+++ tigr_1-4.py
@@ -331,7 +331,7 @@ elif st.session_state.current_step == 6:
                 st.warning("Пожалуйста, выберите событие для каждого указателя времени.")
         
         # Кнопка пропуска
-        func.skip_task(st, index, answ_co, "Задание 2: ")
+        func.skip_task(st, current_block, answ_co, "Задание 2: ")
     
     else:
         st.header("Задание 2 завершено!")
